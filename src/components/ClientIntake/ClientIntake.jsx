77bba1c2f456d6f01d798a7a030dfa2fa4d9f830
Julia Bayless <julia.allistair@gmail.com>
2021-10-28 12:01:13 -0500

    Inputs complete, need handle submit

--- src/components/ClientIntake/ClientIntake.jsx
+++ src/components/ClientIntake/ClientIntake.jsx
@@ -1,19 +1,40 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 
 function ClientIntake() {
-    let [clientIntake, setClientIntake] = useState('');
+    const [clientName, setClientName] = useState('');
+    const [clientAddress, setClientAddress] = useState('');
+    const [clientCity, setClientCity] = useState('');
+    const [clientZip, setClientZip] = useState('');
+
 
     return (
         <form onSubmit={handleSubmit}>
             {/* name */}
-            
+            <input type="text" 
+            value={clientName}
+            placeholder="name"
+            onChange={(event) => setClientName(event.target.value)}
+            />
             {/* street address */}
-
+            <input type="text" 
+            value={clientAddress}
+            placeholder="name"
+            onChange={(event) => setClientAddress(event.target.value)}
+            />
             {/* city */}
-
+            <input type="text" 
+            value={clientCity}
+            placeholder="name"
+            onChange={(event) => setClientCity(event.target.value)}
+            />
             {/* zip */}
-
+            <input type="text" 
+            value={clientZip}
+            placeholder="name"
+            onChange={(event) => setClientZip(event.target.value)}
+            />
             {/* option select for pick vs delivery */}
 
 
